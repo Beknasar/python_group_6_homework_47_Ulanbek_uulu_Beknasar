b883f5343744623e4fcfcf1a7d3c2f4591393e1c
Beknasar <680633@gmail.com>
2020-07-24 00:47:04 +0600

    Добавлен модель Задач

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('new', 'Новая'),
+    ('in_progress', 'В процессе'),
+    ('done', 'Сделано')]
+
+class Tasks(models.Model):
+    description = models.TextField(max_length=500, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=15, choices= STATUS_CHOICES, default='new', verbose_name='Модерация')
+    task_deadline = models.DateField(default=None, null=True, blank=True, verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.title)
+
+class Meta:
+    verbose_name = "Задача"
+    verbose_name_plural = "Задачи"
