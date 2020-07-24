c9cc1cbf90e21a216f099117377713a41f0cfab0
Beknasar <680633@gmail.com>
2020-07-24 12:03:36 +0600

    Добавлена добавлние задачи на сайт.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from webapp.models import Tasks
+from webapp.models import Tasks, STATUS_CHOICES
+from django.http import HttpResponseNotAllowed
 
 def index_view(request):
     data = Tasks.objects.all()
     return render(request, 'index.html', context = {
         'tasks': data
     })
+
+def create_task_view(request):
+    if request.method == 'GET':
+        return render(request, 'task_create.html', context={
+            'status_choices': STATUS_CHOICES
+        })
+    elif request.method == 'POST':
+        #print(request.POST)
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        date = request.POST.get('date')
+        task = Tasks.objects.create(description=description,
+                                    status=status,
+                                    task_deadline=date)
+        context = {'task': task}
+        return render(request, 'task_view.html', context)
+    else:
+        return HttpResponseNotAllowed(
+            permitted_methods=['GET', 'POST'])
